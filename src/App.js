330bbefd40d728a69a710866f4f45ef7f533148a
zoey-lee-korea <wngpsid4014@gmail.com>
2023-11-13 13:30:50 +0900

    study : useState 개념 실습

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
+import { useState } from 'react';
+
 import './App.css';
 
 function Counter({ title, initValue }) {
+  const [count, setCount] = useState(initValue);
+  // useState는 배열이다!
+  // const countState = useState(initValue);
+  // const count = countState[0]
+  // count setCount = countState[1]
+
+  function clickHandler() {
+    setCount(count + 1);
+  }
+
   return (
     <>
       <h1>{title}</h1>
-      <button>+</button>{initValue}
+      <button onClick={clickHandler}>+</button>{count}
     </>
   );
 }
