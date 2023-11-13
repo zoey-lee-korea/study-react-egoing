c2e55a82a808bd0c17b1d1809280f0312ddaf17b
zoey-lee-korea <wngpsid4014@gmail.com>
2023-11-13 14:25:57 +0900

    study: input을 받아 state 변경 구현

--- src/App.js
+++ src/App.js
@@ -4,19 +4,24 @@ import './App.css';
 
 function Counter({ title, initValue }) {
   const [count, setCount] = useState(initValue);
+  const [step, setStep] = useState(1);
 
   const increaseClickHandler = () => {
-    setCount(count + 1);
-  }
+    setCount(count + step);
+  };
   const decreaseClickHandler = () => {
-    setCount(count - 1);
-  }
+    setCount(count - step);
+  };
+  const changeHandler = (e) => { // e: event
+    setStep(Number(e.target.value));
+  };
 
   return (
     <>
       <h1>{title}</h1>
       <button onClick={increaseClickHandler}>+</button>
       <button onClick={decreaseClickHandler}>-</button>
+      <input type="text" value={step} onChange={changeHandler}></input>
       {count}
     </>
   );
