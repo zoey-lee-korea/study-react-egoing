5277ace78a18fa989afbf9636b029793aa97baf4
zoey-lee-korea <wngpsid4014@gmail.com>
2023-11-20 17:14:23 +0900

    study: useParams를 이용한 동적 URL 구현

--- next-day2/app/Control.tsx
+++ next-day2/app/Control.tsx
@@ -1,18 +1,26 @@
 'use client';
 import Link from "next/link";
+import { useParams } from "next/navigation";
 
 export default function Control() {
+    const params = useParams();
+
     return (
         <ul>
             <li>
                 <Link href='/create'>create</Link>
             </li>
-            <li>
-                <Link href={"/update/1"}>update</Link>
-            </li>
-            <li>
-                <button>delete</button>
-            </li>
+            {params.id ? (
+                <>
+                    <li>
+                        <Link href={"/update/" + params.id}>update</Link>
+                    </li>
+                    <li>
+                        <button>delete</button>
+                    </li>
+                </>
+            ) : null
+            }
         </ul>
     )
 }
