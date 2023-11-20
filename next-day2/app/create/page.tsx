8fd1d5481b9d48a458e144a2f79ccbc7eb2a03f9
zoey-lee-korea <wngpsid4014@gmail.com>
2023-11-20 15:41:14 +0900

    study: 게시물 Create 구현 (onSubmit, await fetch)

--- next-day2/app/create/page.tsx
+++ next-day2/app/create/page.tsx
@@ -1,8 +1,35 @@
-// @ts-ignore
+"use client"
+// client component : 사용자가 입력한 정보를 받기 때문
+
+
 export default function Create() {
     return (
         <>
-            <h3>Create</h3>
+            <form onSubmit={async (e: any) => {
+                e.preventDefault(); // submit의 default는 화면 전환 -> prevent로 사용하지 않겠다 선언
+
+                const title = e.target.title.value;
+                const body = e.target.body.value;
+
+                const res = await fetch("http://localhost:9999/posts", {
+                    method: "POST",
+                    headers: {
+                        "Content-Type": "application/json",
+                    },
+                    body: JSON.stringify({ title, body }),
+                })
+                const result = res.json();
+            }}>
+                <p>
+                    <input type="text" name="title" placeholder="제목" />
+                </p>
+                <p>
+                    <textarea name="body" placeholder="내용"></textarea>
+                </p>
+                <p>
+                    <input type="submit" />
+                </p>
+            </form>
         </>
     )
 }
