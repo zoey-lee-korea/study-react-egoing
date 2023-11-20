0aba258efbebad5bf9deeee2dc43d8bfe170b87f
zoey-lee-korea <wngpsid4014@gmail.com>
2023-11-20 16:19:44 +0900

    study: 게시글 Read 기능 구현

--- next-day2/app/read/[id]/page.tsx
+++ next-day2/app/read/[id]/page.tsx
@@ -4,10 +4,13 @@ type ReadProps = {
     };
 };
 
-export default function Read({ params: { id } }: ReadProps) {
+export default async function Read({ params: { id } }: ReadProps) {
+    const res = await fetch("http://localhost:9999/posts/" + id);
+    const post = await res.json();
     return (
         <>
-            <h3>Read {id}</h3>
+            <h2>{post.title} </h2>
+            {post.body}
         </>
     )
 }
