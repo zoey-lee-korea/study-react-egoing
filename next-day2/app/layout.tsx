947cf217588fcd10bb1e25f65053a5d5eef9f03a
zoey-lee-korea <wngpsid4014@gmail.com>
2023-11-20 14:22:35 +0900

    study: 게시물 fetch 구현 (map, useEffect) 및 type 정의

--- next-day2/app/layout.tsx
+++ next-day2/app/layout.tsx
@@ -1,8 +1,13 @@
 "use client";
-import type { Metadata } from 'next'
 import './globals.css'
 import Link from "next/link";
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
+
+type post = {
+  id: string;
+  title: string;
+  body: string;
+};
 
 export default function RootLayout({
   children,
@@ -10,22 +15,30 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
 
+  //
+  const [posts, setPosts] = useState<post[]>([]);
   useEffect(() => {
+    fetch("http://localhost:9999/posts")
+      .then((res) => res.json())
+      .then((data) => {
+        setPosts(data);
+      });
+  }, []);
 
-  })
-
+  //
   return (
     <html lang="en">
       <body>
         <h1><Link href='/'>Main</Link></h1>
         <input type='text' placeholder='search'></input>
         <ul>
-          <li>
-            <Link href='/read/1'>page1</Link>
-          </li>
-          <li>
-            <Link href='/read/2'>page2</Link>
-          </li>
+          {posts.map((post) => {
+            return (
+              <li key={post.id}>
+                <Link href={"/read/" + post.id}>{post.title}</Link>
+              </li>
+            )
+          })}
         </ul>
         <article>{children}</article>
         <ul>
